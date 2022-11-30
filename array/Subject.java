996cdba16d8bf6bae1726d487c3854cf2448e45b
tjddnr7760 <tjddnr7760@naver.com>
2022-11-30 15:17:58 +0900

    docs : 알고리즘용 WBS작성

---
+++ array/Subject.java
@@ -0,0 +1,31 @@
+/** Work BreackDown Structure in algorithm
+ * 
+ *  1. Input
+ *      * 한 줄씩 제출한 사람 번호가 입력된다.
+ * 
+ *  2. Output
+ *      * 제출하지 않은 번호 중 작은수 1
+ *      * 제출하지 않은 번호 중 큰수 1
+ * 
+ *  3. Domain
+ *      * 28개의 숫자를 저장
+ * 
+ *  4. Controller
+ *      1. 숫자를 입력 받는다 - BuffereReader -> String - > Integer 캐스팅
+ *      2. ArrayList<Integer> 로 입력받는다.
+ *      3. 1 ~ 30까지 for문으로 돌면서 없는 숫자 정답 리스트에 푸쉬한다.
+ *      4. 정답 리스트를 정렬한다.
+ *      5. 정답을 출력한다. 
+ *  
+ *  5. Exception
+ *      * 숫자 입력이 28개가 아닌경우
+ *      * 숫자가 1 <= n <= 30 을 벗어나는 경우
+ *      * 자연수가 아닌 경우
+ * 
+ */
+
+public class Subject {
+    public static void main(String[] args) {
+        
+    }
+}
